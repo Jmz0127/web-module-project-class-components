5a0806b2425059b71596716a54b6f668d1133172
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2021-11-29 18:05:23 -0500

    set up dynamic adding of todo list items

--- src/App.js
+++ src/App.js
@@ -4,15 +4,46 @@ class App extends React.Component {
 	// you will need a place to store your state in this component.
 	// design `App` to be the parent component of your application.
 	// this component is going to take care of state, and any change handlers you need to work with your state
-
+	constructor() {
+		super();
+		this.state = {
+			todos: [
+				{
+					task: 'Organize Garage',
+					id: 1528817077286,
+					completed: false
+				},
+				{
+					task: 'Bake Cookies',
+					id: 1528817084358,
+					completed: false
+				},
+				{
+					task: 'Walk Roger',
+					id: 1528817084359,
+					completed: false
+				},
+				{
+					task: 'Take out recycling and trash',
+					id: 1528817084357,
+					completed: false
+				}
+			]
+		};
+	}
 	render() {
+		const { todos } = this.state;
 		return (
 			<div>
 				<h1>Todo List: MVP</h1>
 				<ul>
-					<li>Walk Roger</li>
-					<li>Take out recycling and trash</li>
-					<li>Eat dinner</li>
+					{todos.map((todo) => {
+						return (
+							<li>
+								{todo.task} {todo.completed ? <span>- completed</span> : <span></span>}
+							</li>
+						);
+					})}
 				</ul>
 
 				<form>
